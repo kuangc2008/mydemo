dbc82a0dcaaafb56a9e1846af764f5c369911184
kc <kc@kc-ThinkPad-Edge-E430c.(none)>
2013-12-07 20:50:58 +0800

    原生drawable；  xfermode

---
+++ src/com/example/demo/dec/Shape_Drawable_Activity.java
@@ -0,0 +1,15 @@
+package com.example.demo.dec;
+
+import com.example.demo.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Shape_Drawable_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shape_drawable);
+    }
+}
