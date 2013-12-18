8194962b2ffd9ae7426ec545e42aea1d87cdd2b0
kc <kc@kc-ThinkPad-Edge-E430c.(none)>
2013-12-18 16:00:59 +0800

    GradientDrawable2

---
+++ src/com/example/demo/dec/GradientDrawable.java
@@ -0,0 +1,15 @@
+package com.example.demo.dec;
+
+import com.example.demo.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GradientDrawable extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gradient_drawable_layout);
+    }
+}
