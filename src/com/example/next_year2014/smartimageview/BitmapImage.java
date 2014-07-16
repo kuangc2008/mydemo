e0022f6a49766279f642ccc711d70fae0ffa7021
kuangc2008 <kuangc1987@gmail.com>
2014-07-16 13:55:51 +0800

    smartImageView： 一个从网络设置图片的简单框架

---
+++ src/com/example/next_year2014/smartimageview/BitmapImage.java
@@ -0,0 +1,16 @@
+package com.example.next_year2014.smartimageview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+public class BitmapImage implements SmartImage {
+    private Bitmap bitmap;
+
+    public BitmapImage(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+
+    public Bitmap getBitmap(Context context) {
+        return bitmap;
+    }
+}
---
+++ src/com/example/next_year2014/smartimageview/SmartImage.java
@@ -0,0 +1,8 @@
+package com.example.next_year2014.smartimageview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+public interface SmartImage {
+    public Bitmap getBitmap(Context context);
+}
