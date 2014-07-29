556873b6658a767cfd1b37bf29269b8965283cff
kuangc2008 <kuangc1987@gmail.com>
2014-07-29 10:17:47 +0800

    animation2 ，可以加入动画（但是setTranlationY不起作用哦）

--- src/com/example/next_year2014/MyAnimationActivity.java
+++ src/com/example/next_year2014/MyAnimationActivity.java
@@ -21,6 +21,8 @@ public class MyAnimationActivity extends Activity implements View.OnClickListene
 
         findViewById(R.id.right_move).setOnClickListener(this);
         findViewById(R.id.rotation_x).setOnClickListener(this);
+        findViewById(R.id.rotation_z).setOnClickListener(this);
+        findViewById(R.id.scaleY).setOnClickListener(this);
         centerView  = findViewById(R.id.center);
         centerView.setOnClickListener(this);
 
@@ -33,15 +35,18 @@ public class MyAnimationActivity extends Activity implements View.OnClickListene
             case R.id.right_move:
                 ap = AnimatorProxy.wrap(centerView);
                 ap.setTranslationX(ap.getTranslationX() + 20);
+                ap.setTranslationY(ap.getTranslationY() + 20);
+         //       centerView.setTranslationX(centerView.getTranslationX()+20);
 
 
                 TimeAnimation ta = new TimeAnimation();
-                ta.setDuration(1000);
+                ta.setDuration(4000);
+                ta.setValue(500, 1000);
                 centerView.startAnimation(ta);
                 ta.setTimeAnimationListener(new TimeAnimationListener() {
                     @Override
-                    public void onAnimationUpdate(float interpolatedTime) {
-                        Log.v("kcc", "intertime-->" + interpolatedTime);
+                    public void onAnimationUpdate(float updatevalue) {
+                        Log.v("kcc", this+ "intertime-->" + updatevalue);
                     }
                 });
                 break;
@@ -52,24 +57,39 @@ public class MyAnimationActivity extends Activity implements View.OnClickListene
                 ap = AnimatorProxy.wrap(centerView);
                 ap.setRotationX(ap.getRotationX() + 20);
                 break;
+            case R.id.rotation_z:
+                ap = AnimatorProxy.wrap(centerView);
+                ap.setRotation(ap.getRotation() + 20);
+                break;
+            case R.id.scaleY:
+                ap = AnimatorProxy.wrap(centerView);
+                ap.setScaleY((float) (ap.getScaleY() + 0.2));
+                break;
         }
     }
 
 
     private class TimeAnimation extends Animation {
         TimeAnimationListener tl = null;
+        float startValue = 0f;
+        float endValue = 1f;
         @Override
         protected void applyTransformation(float interpolatedTime, Transformation t) {
             super.applyTransformation(interpolatedTime, t);
-            tl.onAnimationUpdate(interpolatedTime);
+            tl.onAnimationUpdate(interpolatedTime * (endValue - startValue) + startValue);
         }
 
         public void setTimeAnimationListener(TimeAnimationListener tl) {
             this.tl = tl;
         }
+
+        public void setValue(float start, float end) {
+            startValue = start;
+            endValue = end;
+        }
     }
 
     private interface TimeAnimationListener {
-        void onAnimationUpdate(float interpolatedTime);
+        void onAnimationUpdate(float updatevalue);
     }
 }
