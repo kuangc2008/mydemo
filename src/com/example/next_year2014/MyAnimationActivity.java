a5a2bf52421c9549ad9525a639f40a8628d5d67b
kuangc2008 <kuangc1987@gmail.com>
2014-07-29 11:10:36 +0800

    传统动画，与2.3中采用4.0 api动画的对比.

--- src/com/example/next_year2014/MyAnimationActivity.java
+++ src/com/example/next_year2014/MyAnimationActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Toast;
 
 import com.example.demo.R;
@@ -23,6 +24,8 @@ public class MyAnimationActivity extends Activity implements View.OnClickListene
         findViewById(R.id.rotation_x).setOnClickListener(this);
         findViewById(R.id.rotation_z).setOnClickListener(this);
         findViewById(R.id.scaleY).setOnClickListener(this);
+        findViewById(R.id.animation_test).setOnClickListener(this);
+        findViewById(R.id.animation_old).setOnClickListener(this);
         centerView  = findViewById(R.id.center);
         centerView.setOnClickListener(this);
 
@@ -33,20 +36,22 @@ public class MyAnimationActivity extends Activity implements View.OnClickListene
         AnimatorProxy ap  = null;
         switch (v.getId()) {
             case R.id.right_move:
-                ap = AnimatorProxy.wrap(centerView);
-                ap.setTranslationX(ap.getTranslationX() + 20);
-                ap.setTranslationY(ap.getTranslationY() + 20);
-         //       centerView.setTranslationX(centerView.getTranslationX()+20);
 
+//                ap.setTranslationX(ap.getTranslationX() + 20);
+//                ap.setTranslationY(ap.getTranslationY() + 20);
+         //       centerView.setTranslationX(centerView.getTranslationX()+20);
 
                 TimeAnimation ta = new TimeAnimation();
                 ta.setDuration(4000);
-                ta.setValue(500, 1000);
-                centerView.startAnimation(ta);
+                ta.setValue(0, 500);
+                ((View)centerView.getParent()).startAnimation(ta);
                 ta.setTimeAnimationListener(new TimeAnimationListener() {
                     @Override
                     public void onAnimationUpdate(float updatevalue) {
                         Log.v("kcc", this+ "intertime-->" + updatevalue);
+                        AnimatorProxy ap = AnimatorProxy.wrap(centerView);
+                        ap.setTranslationX(updatevalue);
+                        ap.setTranslationY(updatevalue);
                     }
                 });
                 break;
@@ -64,6 +69,20 @@ public class MyAnimationActivity extends Activity implements View.OnClickListene
             case R.id.scaleY:
                 ap = AnimatorProxy.wrap(centerView);
                 ap.setScaleY((float) (ap.getScaleY() + 0.2));
+                break;
+            case R.id.animation_test:
+                //不起作用
+                ap = AnimatorProxy.wrap(centerView);
+                ap.setTranslationY(300);
+                ap.setTranslationX(300);
+                ap.setDuration(300);
+                centerView.startAnimation(ap);
+                break;
+            case R.id.animation_old:
+                TranslateAnimation ta2 = new TranslateAnimation(TranslateAnimation.ABSOLUTE, 0, TranslateAnimation.ABSOLUTE, 500,
+                        TranslateAnimation.ABSOLUTE, 0, TranslateAnimation.ABSOLUTE, 500);
+                ta2.setDuration(4000);
+                centerView.startAnimation(ta2);
                 break;
         }
     }
