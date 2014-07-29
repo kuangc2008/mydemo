abf9d38fe9d229f2d3afdd074cff4fff2d823b0d
kuangc2008 <kuangc1987@gmail.com>
2014-07-29 21:49:50 +0800

    已有动画效果.

--- src/com/example/next_year2014/YoutubeLayout.java
+++ src/com/example/next_year2014/YoutubeLayout.java
@@ -2,6 +2,7 @@ package com.example.next_year2014;
 
 import android.content.Context;
 import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -18,8 +19,6 @@ public class YoutubeLayout extends ViewGroup {
     private View mDescView;
     private int mDragRange;
 
-
-
     public YoutubeLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         mDragHelper = ViewDragHelper.create(this, 1f, new DragHelperCallback());
@@ -32,6 +31,8 @@ public class YoutubeLayout extends ViewGroup {
     }
 
 
+    private float mInitialMotionX;
+    private float mInitialMotionY;
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         Log.v("kcc", "onInterceptTouchEvent-->");
@@ -40,7 +41,21 @@ public class YoutubeLayout extends ViewGroup {
             mDragHelper .cancel();
             return false ;
         }
-        return mDragHelper.shouldInterceptTouchEvent(ev );
+
+        final float x = ev.getX();
+        final float y = ev.getY();
+        boolean interceptTap = false;
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN: {
+                mInitialMotionX = x;
+                mInitialMotionY = y;
+                interceptTap = mDragHelper.isViewUnder(mHeaderView, (int)x ,(int)y);
+                break;
+            }
+        }
+
+        return mDragHelper.shouldInterceptTouchEvent(ev) || interceptTap;
 
     }
 
@@ -48,7 +63,52 @@ public class YoutubeLayout extends ViewGroup {
     public boolean onTouchEvent(MotionEvent event) {
         Log.v("kcc", "onToucheEvent-->");
         mDragHelper.processTouchEvent(event);
+
+        final int action = event.getAction();
+        final float x = event.getX();
+        final float y = event.getY();
+
+        boolean isHeaderViewUnder = mDragHelper.isViewUnder(mHeaderView, (int) x, (int)y);
+        switch (action & MotionEventCompat.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: {
+                mInitialMotionY = x;
+                mInitialMotionY = y;
+                break;
+            }
+
+            case MotionEvent.ACTION_UP:
+                final float dx = x - mInitialMotionX;
+                final float dy = y - mInitialMotionY;
+                final int slop = mDragHelper.getTouchSlop();
+                if(dx * dx + dy * dy < slop * slop && isHeaderViewUnder) {
+                    if(mDragOffset == 0) {
+                        smoothSlideTo(1f);
+                    } else {
+                        smoothSlideTo(0f);
+                    }
+                }
+                break;
+        }
+
         return true;   //因为没有return true，所以一直点击没反应。 因为点击事件，让子控件拿走了。
+    }
+
+    boolean smoothSlideTo(float slideOffset) {
+        final int topBound = getPaddingTop();
+        int y = (int)  (topBound + slideOffset * mDragRange);
+
+        if(mDragHelper.smoothSlideViewTo(mHeaderView, mHeaderView.getLeft(), y)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void computeScroll() {
+        if(mDragHelper.continueSettling(true)) {  //持续触发onViewPositionChnaged
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
     }
 
     @Override
@@ -62,9 +122,10 @@ public class YoutubeLayout extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        Log.v("kcc", "onLayout-->" + " t-->" + t + "   b-->" + b);
-        mDragRange = getHeight() - mHeaderView.getHeight();
 
+        mDragRange = getHeight() - mHeaderView.getHeight();
+        Log.v("kcc", "onLayout-->" + " t-->" + t + "   b-->" + b + "   getHeight-->" + getHeight()
+                +"  headderViewHeight-->" + mHeaderView.getHeight());
         mHeaderView.layout(0, mTop, r, mTop + mHeaderView.getMeasuredHeight());
 
         mDescView.layout(0, mTop + mHeaderView.getMeasuredHeight(), r, mTop + b);
@@ -77,20 +138,20 @@ public class YoutubeLayout extends ViewGroup {
         @Override
         public boolean tryCaptureView(View child, int pointerId) {
             Log.v("kcc", "tryCaptureView-->" + child);
-            return true;
+            return child == mHeaderView;
         }
 
 
         @Override
         public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
-            Log.v("kcc", "onViewPositionChanged-->" + top + "   dy--." + dy);
+
             mTop = top;
             mDragOffset = (float) top / mDragRange;
-
+            Log.v("kcc", "onViewPositionChanged-->" + top + "   dy--." + dy+"   mDragOffset-->" + mDragOffset);
             mHeaderView.setPivotX(mHeaderView.getWidth());
             mHeaderView.setPivotY(mHeaderView.getHeight());
             mHeaderView.setScaleX(1 - mDragOffset / 2);
-            mHeaderView.setScaleY(1 - mDragOffset / 2);
+            mHeaderView.setScaleY(1 - mDragOffset / 2);   //scale不会影响到height的大小
 
             mDescView.setAlpha(1 - mDragOffset);
 
@@ -115,13 +176,19 @@ public class YoutubeLayout extends ViewGroup {
         }
 
         @Override
+        public int getViewHorizontalDragRange(View child) {   //调用smoothScroolTo时，会回调这个
+            Log.v("kcc", "getViewHorizontalDragRange-->" + child);
+            return super.getViewHorizontalDragRange(child);
+        }
+
+        @Override
         public void onViewReleased(View releasedChild, float xvel, float yvel) {
             Log.v("kcc", "onViewReleased-->" + yvel);
             int top = getPaddingTop();
             if(yvel>0 || (yvel == 0 && mDragOffset > 0.5f)) {
                 top += mDragRange;
             }
-            mDragHelper.settleCapturedViewAt(releasedChild.getLeft(), top);
+            mDragHelper.settleCapturedViewAt(releasedChild.getLeft(), top);   //难道是因为realease么？
             invalidate();
         }
     }
